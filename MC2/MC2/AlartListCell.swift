3e9855b765e60ff767f7f9728e30ff70ecf5c7e5
songmoro <97587585+songmoro@users.noreply.github.com>
2023-05-12 14:56:59 +0900

    Add files via upload

---
+++ MC2/MC2/AlartListCell.swift
@@ -0,0 +1,42 @@
+//
+//  AlartListCell.swift
+//  trees
+//
+//  Created by 최진용 on 2023/05/11.
+//
+
+import SwiftUI
+
+struct AlartListCell: View {
+    var text: String
+    var body: some View {
+        HStack {
+            Text("\(text)")
+            Spacer()
+            Button {
+                print("ok")
+            } label: {
+                Text("수락")
+                    .foregroundColor(.white)
+            }.frame(width: 60, height: 30)
+                .background(Color.accentColor)
+                .cornerRadius(8)
+            Button {
+                print("cancle")
+            } label: {
+                Text("거절")
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+            }.frame(width: 60, height: 30)
+                .background(Color.gray)
+                .cornerRadius(8)
+        }
+    }
+}
+
+struct AlartView_Preview: PreviewProvider {
+    static var previews: some View {
+        let text = "textwefniweofnwapqegnioqwbeo"
+        AlartListCell(text: text)
+    }
+}
