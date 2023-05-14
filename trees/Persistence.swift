114618e9c2bb0ff95a7834535dacd7c3422ac7c4
ckyeon <qsky8520@gmail.com>
2023-05-14 21:26:37 +0900

    change database private to public

--- trees/Persistence.swift
+++ trees/Persistence.swift
@@ -6,51 +6,50 @@
 //
 
 import CoreData
+import Foundation
+import CloudKit
+import SwiftUI
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
 
     let container: NSPersistentCloudKitContainer
 
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "trees")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        
+        guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("###\(#function): Failed to retrieve ta persistent store description.")
         }
+        description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+        description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+        
+        let options = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.it794613.trees")
+        options.databaseScope = .public
+        
+        description.cloudKitContainerOptions = options
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
+        #if DEBUG
+        do {
+            try container.initializeCloudKitSchema()
+        } catch {
+            print("=======================================")
+            print("\(#function): initializeCloudKitSchema: \(error)")
+            print("=======================================")
+        }
+        #endif
+        
+        do {
+            try container.viewContext.setQueryGenerationFrom(.current)
+        } catch {
+            fatalError("#\(#function): Failed to pin viewContext to the current generation:\(error)")
+        }
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
